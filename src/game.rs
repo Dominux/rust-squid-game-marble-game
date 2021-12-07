ad4de743245edce4279be1badf241b14a8a0ad89
Dominux <kir.gimranov@gmail.com>
2021-12-07 21:15:06 +0300

    set kinda proper way to define if the player riddler or guesser

--- src/game.rs
+++ src/game.rs
@@ -1,17 +1,28 @@
 const INIT_MARBLES_AMOUNT: u8 = 100;
 
-#[derive(Debug, Default)]
-pub struct Player {
-    name: String,
-    marbles_amount: u8,
+#[derive(Debug)]
+pub enum PlayerMovingPosition {
+	Riddler,
+	Guesser,
 }
 
-#[derive(Debug, Default)]
+
+#[derive(Debug)]
+pub struct Player {
+    pub name: String,
+    pub marbles_amount: u8,
+	pub moving_position: PlayerMovingPosition,
+}
+
+// impl Player {
+// 	pub fn make_move(&self, game: &Game) {
+// 	}
+// }
+
+#[derive(Debug)]
 pub struct Game {
     pub player1: Player,
     pub player2: Player,
-	current_riddler: Option<Player>,
-	current_guesser: Option<Player>,
 }
 
 impl Game {
@@ -19,16 +30,13 @@ impl Game {
         let player1 = Player {
             name: player_1_name,
             marbles_amount: INIT_MARBLES_AMOUNT,
+			moving_position: PlayerMovingPosition::Riddler,
         };
         let player2 = Player {
             name: player_2_name,
             marbles_amount: INIT_MARBLES_AMOUNT,
+			moving_position: PlayerMovingPosition::Guesser,
         };
-        Game { player1, player2, ..Default::default() }
+        Game { player1, player2 }
     }
-
-	// / Represent a single game move
-	// pub fn game_move(&self) {
-
-	// }
 }
